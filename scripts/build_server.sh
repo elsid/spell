952f4a8e78bd9f53290cdfae9cdc27a836b6b457
elsid <elsid.mail@gmail.com>
2021-05-06 18:13:06 +0200

    Update docker rust version to 1.51

--- scripts/build_server.sh
+++ scripts/build_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-docker run --rm --tty --interactive --user "$(id -u)":"$(id -g)" --volume "${PWD}":/code --workdir /code rust:1.50 \
+docker run --rm --tty --interactive --user "$(id -u)":"$(id -g)" --volume "${PWD}":/code --workdir /code rust:1.51 \
   cargo build --release --target-dir=docker_target
